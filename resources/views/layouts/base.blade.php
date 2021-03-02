de4165b8cd7e1b62c11ab391fbaa2b4ab158dbea
sayrsalad <sayrsalad@users.noreply.github.com>
2021-03-02 17:22:31 +0800

    Initial commit

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		@include('layouts.header')
+	</head>
+	
+	<body>
+		@include('layouts.navbar ')
+		</br>
+		@yield('body')
+	</body>
+</html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,42 @@
+			<!-- resources/views/layouts/header.blade.php -->
+
+			<title>@yield('title')</title>
+			<link rel="icon" href="{!! asset('images/web/clapper2.png') !!}" type="image/png" />
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+
+			<!-- CSRF Token -->
+			<meta name="csrf-token" content="{{ csrf_token() }}">
+			<script type="text/javascript">var token = '{{ csrf_token() }}';</script>
+
+			<!-- Fonts -->
+			<link rel="dns-prefetch" href="//fonts.gstatic.com">
+			<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+			<link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
+
+			<!-- Styles -->
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker3.min.css" integrity="sha512-rxThY3LYIfYsVCWPCW9dB0k+e3RZB39f23ylUYTEuZMDrN/vRqLdaCBo/FbvVT6uC2r0ObfPzotsfKF9Qc5W5g==" crossorigin="anonymous" />
+			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+			<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+			<link href="{{ asset('css/main.css') }}" rel="stylesheet">			
+
+			<!-- Scripts -->
+			<!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+			<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/jquery.validate.js"></script>
+			<script src="{{asset('js/movies.js')}}"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js" integrity="sha512-T/tUfKSV1bihCnd+MxKD0Hm1uBBroVYBOYSk1knyvQ9VyZJpc/ALb4P0r6ubwVPSGB2GvjeoMAJJImBG12TiaQ==" crossorigin="anonymous"></script>
+			<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+			<script src="{{asset('js/popper.min.js')}}"></script>
+			<script src="{{asset('js/bootbox.min.js')}}"></script>
+
+
+
+
+
+
+
