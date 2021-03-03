a22afba8342f84153d351a8ba98989f9e1aa5c03
sayrsalad <sayrsalad@users.noreply.github.com>
2021-03-03 17:18:28 +0800

    Update weather.js

--- public/js/weather.js
+++ public/js/weather.js
@@ -1,1 +1,19 @@
-a
+$(document).ready(function () {
+
+	$("#submitCity").click(function(){
+		return getWeather();
+	});
+
+});
+
+function getWeather(){
+	var city = $("#city").val();
+
+	if (city != '') {
+		$.ajax({
+			url: 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric'+'&appid=5d33ff84ea5610b1bfa28790de231985'
+		});
+	} else {
+		$("#error").html("<div>Please input a valid city.</div>");
+	}
+}
