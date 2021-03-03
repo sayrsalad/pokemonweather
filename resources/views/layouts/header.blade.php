7e4afa75c3c6081b609ac41965c6f14080afb7a6
sayrsalad <sayrsalad@users.noreply.github.com>
2021-03-03 17:07:35 +0800

    js

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
 			<!-- resources/views/layouts/header.blade.php -->
 
 			<title>@yield('title')</title>
-			<link rel="icon" href="{!! asset('images/web/clapper2.png') !!}" type="image/png" />
+			<link rel="icon" href="{!! asset('images/icon2.png') !!}" type="image/png" />
 			<meta charset="UTF-8">
 			<meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -25,7 +25,7 @@
 			<!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 			<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/jquery.validate.js"></script>
-			<script src="{{asset('js/main.js')}}"></script>
+			<script src="{{asset('js/weather.js')}}"></script>
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js" integrity="sha512-T/tUfKSV1bihCnd+MxKD0Hm1uBBroVYBOYSk1knyvQ9VyZJpc/ALb4P0r6ubwVPSGB2GvjeoMAJJImBG12TiaQ==" crossorigin="anonymous"></script>
