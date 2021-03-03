8a011b99f82fb53d778bed2d920df67765e0e12e
sayrsalad <sayrsalad@users.noreply.github.com>
2021-03-03 16:26:07 +0800

    Update navbar.blade.php

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#"><span class="box-text top-logo">POKEMON</span><span class="box-text bot-logo">WEATHER</span></a>
+  <a class="navbar-brand" href="{{url('/')}}"><span class="box-text top-logo">POKEMON</span><span class="box-text bot-logo">WEATHER</span></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
