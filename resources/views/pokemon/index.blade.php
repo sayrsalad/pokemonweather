c9fb32f2edf0767d1fad4aa15042e43ca4773733
DiamondShy <tenaciousshyo@gmail.com>
2021-03-07 14:31:04 +0800

    wrarw

--- resources/views/pokemon/index.blade.php
+++ resources/views/pokemon/index.blade.php
@@ -1,3 +1,9 @@
 @section('title', 'Open Weather')
 @extends('layouts.base')
 @section('body')
+
+
+<h1>PokeDex</h1>
+<div id="poke_container" class="poke-container"></div>
+<link rel="stylesheet" href="pokemon.css"/>
+<script language = "JavaScript" src="pokemon.js"></script>
